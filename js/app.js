2eb16f6401983d235d2f7205e3d983a996d12b88
Craig <wcrmcgowan@gmail.com>
2025-05-14 13:57:14 -0700

    updated backend link

--- js/app.js
+++ js/app.js
@@ -25,7 +25,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const noHistoryP = document.getElementById('no-history');
 
     // Configuration
-    const API_BASE_URL = 'https://streamzine-service-122411196232.us-west1.run.app';
+    //const API_BASE_URL = 'http://localhost:8000';
+    const API_BASE_URL = 'https://streamzine.onrender.com';
     const MAX_HISTORY_ITEMS = 5;
 
     // State
